81dc026f4b514665108c06e44a00b3eafd1c125a
VRAJESH N PANDYA <npandyavrajesh31@gmail.com>
2025-08-17 22:25:51 +0530

    base url

--- frontend/src/store/useAuth.js
+++ frontend/src/store/useAuth.js
@@ -3,7 +3,7 @@ import axiosInstance from "../lib/axios.js";
 import { toast } from "react-hot-toast";
 import { io } from "socket.io-client";
 
-const BASE_URL = "http://localhost:5001"; // or your backend URL
+const BASE_URL = "https://chat-app-yqx9.onrender.com"; // or your backend URL
 
 const useAuth = create((set, get) => ({
   authuser: null,
