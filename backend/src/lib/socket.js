1cc01ad6f1d525c04deba2c1b4ad5fb178231d4e
VRAJESH N PANDYA <npandyavrajesh31@gmail.com>
2025-08-18 16:11:48 +0530

    vercel erors

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -10,6 +10,7 @@ const io = new Server(server, {
     cors: {
         origin: ["https://chat-app-pearl-phi.vercel.app"],
         methods: ["GET", "POST"],
+        credentials: true,
     },
 });
 
