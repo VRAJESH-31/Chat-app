e6e11f443550efba5576da3bd8f69335f3cdd073
VRAJESH N PANDYA <npandyavrajesh31@gmail.com>
2025-08-18 15:07:25 +0530

    Last push

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-    baseURL: 'http://localhost:5001/api', // Adjust the base URL as needed
+    baseURL: 'https://chat-app-yqx9.onrender.com', // Adjust the base URL as needed
     withCredentials: true, // Include credentials for cross-origin requests
 });
 
