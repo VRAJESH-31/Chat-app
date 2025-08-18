f2d7d21668531eed9548a246242f1235ce66e1a4
VRAJESH N PANDYA <npandyavrajesh31@gmail.com>
2025-08-18 15:45:00 +0530

    vercel erors

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -7,8 +7,8 @@ export const generateToken = (userId, res) => {
     res.cookie('jwt', token, {
         maxAge: 7 * 24 * 60 * 60 * 1000, 
         httpOnly: true,
-        secure: process.env.NODE_ENV === 'development',
-        sameSite: 'strict',
+        secure: true, 
+        sameSite: 'none', 
     });
     return token;
 }
